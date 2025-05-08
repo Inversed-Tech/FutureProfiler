056d431ff9b6e4869c7112abd1ea8923ae1b9e08
Stuart Woodbury <stuart@inversed.tech>
2025-05-08 09:47:20 -0400

    custom waker not needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
-
-mod custom_waker;
 
 pub struct AsyncTracer<T, R>
 where
     T: Future<Output = R> + Send,
 {
+    // used to measure the total time that this future existed
+    start: Instant,
+    // measures the time spent not sleeping
     duration: Duration,
+    // the future of interest. has to be pinned
     future: Pin<Box<T>>,
 }
 
@@ -21,6 +21,7 @@ where
 {
     pub fn new(future: T) -> Self {
         Self {
+            start: Instant::now(),
             duration: Duration::ZERO,
             future: Box::pin(future),
         }
@@ -37,17 +38,10 @@ where
     ///  The `this` variable must not have any data moved out of it.
     ///  It also must not be invalidated.
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let was_woken = Arc::new(AtomicBool::new(false));
-        let wrapped_waker = custom_waker::with_flag(was_woken.clone(), cx.waker().clone());
-        let mut new_cx = Context::from_waker(&wrapped_waker);
-
         let start = Instant::now();
         let this = unsafe { self.get_unchecked_mut() };
-        let r = this.future.as_mut().poll(&mut new_cx);
-        if !matches!(r, Poll::Pending) || was_woken.load(Ordering::SeqCst) {
-            let elapsed = start.elapsed();
-            this.duration += elapsed;
-        }
+        let r = this.future.as_mut().poll(cx);
+        this.duration += start.elapsed();
         r
     }
 }
@@ -58,7 +52,7 @@ mod tests {
     use std::task::Poll;
 
     #[tokio::test]
-    async fn test_async_tracer_duration() {
+    async fn test_async_tracer_duration1() {
         let future = async {
             tokio::time::sleep(Duration::from_millis(100)).await;
             std::thread::sleep(Duration::from_millis(101));
@@ -83,5 +77,40 @@ mod tests {
 
         assert!(tracer.duration <= Duration::from_millis(103));
         assert!(tracer.duration >= Duration::from_millis(101));
+    }
+
+    #[tokio::test]
+    async fn test_async_tracer_duration2() {
+        let future = async {
+            std::thread::sleep(Duration::from_millis(101));
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            std::thread::sleep(Duration::from_millis(10));
+            tokio::time::sleep(Duration::from_millis(20)).await;
+            std::thread::sleep(Duration::from_millis(30));
+            42
+        };
+
+        let mut tracer = AsyncTracer::new(future);
+        let waker = futures::task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+
+        match Pin::new(&mut tracer).poll(&mut cx) {
+            Poll::Pending => {}
+            Poll::Ready(_) => panic!("Future should not be ready yet"),
+        }
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        loop {
+            match Pin::new(&mut tracer).poll(&mut cx) {
+                Poll::Ready(_output) => break,
+                Poll::Pending => {
+                    tokio::time::sleep(Duration::from_millis(5)).await;
+                }
+            }
+        }
+
+        assert!(tracer.duration <= Duration::from_millis(142));
+        assert!(tracer.duration >= Duration::from_millis(141));
     }
 }
